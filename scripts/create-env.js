d2b80156b0d8529924a2a9b85f5a9ac1848db3f5
JUAN-BH <juanmita_bh@hotmail.com>
2022-09-27 08:01:25 -0500

    modificando create-env2

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-fs.writeFileSync("./.env", `WEATHERAPI=${process.env.WEATHERAPI}\n`);
-// fs.writeFileSync("./.env", `LOCATIONAPI=${process.env.LOCATIONAPI}\n`);
+fs.writeFileSync("./.env", `WEATHERAPI=${process.env.WEATHERAPI}`);
+fs.writeFileSync("./.env", `LOCATIONAPI=${process.env.LOCATIONAPI}\n`);
