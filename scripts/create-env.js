f1a5a26f62ba624e75b7bb635911d327b34dc78e
JUAN-BH <juanmita_bh@hotmail.com>
2022-09-27 07:20:18 -0500

    cambiando rutas

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-fs.writeFileSync("./.env", `WEATHER_API=${process.env.WEATHER_API}\n`);
-fs.writeFileSync("./.env", `LOCATION_API=${process.env.LOCATION_API}\n`);
+fs.writeFileSync("../.env", `WEATHER_API=${process.env.WEATHER_API}\n`);
+fs.writeFileSync("../.env", `LOCATION_API=${process.env.LOCATION_API}\n`);
